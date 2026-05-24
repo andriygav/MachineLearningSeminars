82cd0182a1cefc2bdd366d835f3316fbb155d267
andriygav <andriygav@users.noreply.github.com>
2026-05-24 17:30:46 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.a6d9abb1354cc68e7be87dffc37e3549.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5c0640eba4b1b95d417e41e22249ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "611abab0cd2efde3744c",
+    "url": "./static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "70d8a52784d00b401294",
+    "url": "./static/css/main.b57a7b01.chunk.css"
+  },
+  {
+    "revision": "611abab0cd2efde3744c",
+    "url": "./static/js/2.d46c2fc5.chunk.js"
+  },
+  {
+    "revision": "a4b056b1a23e371e60ffa5fcd08b33e0",
+    "url": "./static/js/2.d46c2fc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70d8a52784d00b401294",
+    "url": "./static/js/main.372afe1e.chunk.js"
+  },
+  {
+    "revision": "d8d2e0e7d12a79193211",
+    "url": "./static/js/runtime-main.35bc6503.js"
+  },
+  {
+    "revision": "fb62fd0af9537dbf483fd388494b84ac",
+    "url": "./static/media/binder_logo.fb62fd0a.png"
+  }
+]);
---
+++ static/index.md
@@ -0,0 +1,6 @@
+# Notebook classroom
+
+This is the description of the dummy classroom. Replace this page by 
+adding a `index.md` to your root directory.
+Add a propper description of the class and give some insight what the students can
+find here.
