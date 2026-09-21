4afa98b4b79a73b69e058212253c776572c03872
andriygav <andriygav@users.noreply.github.com>
2026-09-21 17:07:24 +0000

    Deploy to GitHub pages

---
+++ configuration.js
@@ -0,0 +1,271 @@
+var Config = {
+    title: "Machine Learning Seminars",
+    url: "https://andriygav.github.io/MachineLearningSeminars/",
+    navigation: [
+        {
+            label: "Home",
+            link: "readme.htlm",
+            type: "static"
+        },
+        {
+            label: "Notebooks",
+            navigation: [
+                {
+                    label: "Introduction",
+                    link: "sem1.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem1/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem1/main.ipynb"
+                },
+                {
+                    label: "Linear methods",
+                    link: "sem2.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem2/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem2/main.ipynb"
+                },
+                {
+                    label: "Neural networks",
+                    link: "sem3.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem3/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem3/main.ipynb"
+                },
+                {
+                    label: "Metric methods",
+                    link: "sem4.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem4/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem4/main.ipynb"
+                },
+                {
+                    label: "SVM",
+                    link: "sem5.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem5/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem5/main.ipynb"
+                },
+                {
+                    label: "PCA",
+                    link: "sem6.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem6/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem6/main.ipynb"
+                },
+                {
+                    label: "Nonlinear regression",
+                    link: "sem7.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem7/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem7/main.ipynb"
+                },
+                {
+                    label: "Feature selection",
+                    link: "sem8.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem8/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem8/main.ipynb"
+                },
+                {
+                    label: "Logical methods",
+                    link: "sem9.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem9/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem9/main.ipynb"
+                },
+                {
+                    label: "Association Rules",
+                    link: "sem10.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem10/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem10/main.ipynb"
+                },
+                {
+                    label: "Boosting and Bagging",
+                    link: "sem11.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem11/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem11/main.ipynb"
+                },
+                {
+                    label: "Gradient boosting",
+                    link: "sem12.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem12/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem12/main.ipynb"
+                },
+                {
+                    label: "Density recovery",
+                    link: "sem13.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem13/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem13/main.ipynb"
+                },
+                {
+                    label: "Clustering methods",
+                    link: "sem14.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem14/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem14/main.ipynb"
+                },
+                {
+                    label: "Deep Neural Networks",
+                    link: "sem15.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem15/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem15/main.ipynb"
+                },
+                {
+                    label: "Autoencoder",
+                    link: "sem16.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem16/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem16/main.ipynb"
+                },
+                {
+                    label: "Vector representations",
+                    link: "sem17.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem17/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem17/main.ipynb"
+                },
+                {
+                    label: "Attention is all you need",
+                    link: "sem18.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem18/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem18/main.ipynb"
+                },
+                {
+                    label: "Thematic modeling",
+                    link: "sem19.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem19/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem19/main.ipynb"
+                },
+                {
+                    label: "Ranking task",
+                    link: "sem21.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem21/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem21/main.ipynb"
+                },
+                {
+                    label: "Recommender systems",
+                    link: "sem22.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem22/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem22/main.ipynb"
+                },
+                {
+                    label: "Time series",
+                    link: "sem23.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem23/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem23/main.ipynb"
+                },
+                {
+                    label: "Online learning",
+                    link: "sem24.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem24/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem24/main.ipynb"
+                },
+                {
+                    label: "Reinforcement learning",
+                    link: "sem25.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem25/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem25/main.ipynb"
+                },
+                {
+                    label: "Active learning",
+                    link: "sem26.html",
+                    github: "https://github.com/andriygav/MachineLearningSeminars/blob/master/sem26/main.ipynb",
+                    binder: "https://colab.research.google.com/github/andriygav/MachineLearningSeminars/blob/master/sem26/main.ipynb"
+                }
+            ]
+        },
+        {
+            label: "Presentations",
+            navigation: [
+                {
+                    label: "Introduction",
+                    link: "sem1.slides.html",
+                },
+                {
+                    label: "Linear methods",
+                    link: "sem2.slides.html",
+                },
+                {
+                    label: "Neural networks",
+                    link: "sem3.slides.html",
+                },
+                {
+                    label: "Metric methods",
+                    link: "sem4.slides.html",
+                },
+                {
+                    label: "SVM",
+                    link: "sem5.slides.html",
+                },
+                {
+                    label: "PCA",
+                    link: "sem6.slides.html",
+                },
+                {
+                    label: "Nonlinear regression",
+                    link: "sem7.slides.html",
+                },
+                {
+                    label: "Feature selection",
+                    link: "sem8.slides.html",
+                },
+                {
+                    label: "Logical methods",
+                    link: "sem9.slides.html",
+                },
+                {
+                    label: "Association Rules",
+                    link: "sem10.slides.html",
+                },
+                {
+                    label: "Boosting and Bagging",
+                    link: "sem11.slides.html",
+                },
+                {
+                    label: "Gradient boosting",
+                    link: "sem12.slides.html",
+                },
+                {
+                    label: "Density recovery",
+                    link: "sem13.slides.html",
+                },
+                {
+                    label: "Clustering methods",
+                    link: "sem14.slides.html",
+                },
+                {
+                    label: "Deep Neural Networks",
+                    link: "sem15.slides.html",
+                },
+                {
+                    label: "Autoencoder",
+                    link: "sem16.slides.html",
+                },
+                {
+                    label: "Vector representations",
+                    link: "sem17.slides.html",
+                },
+                {
+                    label: "Attention is all you need",
+                    link: "sem18.slides.html",
+                },
+                {
+                    label: "Thematic modeling",
+                    link: "sem19.slides.html",
+                },
+                {
+                    label: "Ranking task",
+                    link: "sem21.slides.html",
+                },
+                {
+                    label: "Recommender systems",
+                    link: "sem22.slides.html",
+                },
+                {
+                    label: "Time series",
+                    link: "sem23.slides.html",
+                },
+                {
+                    label: "Online learning",
+                    link: "sem24.slides.html",
+                },
+                {
+                    label: "Reinforcement learning",
+                    link: "sem25.slides.html",
+                },
+                {
+                    label: "Active learning",
+                    link: "sem26.slides.html",
+                }
+            ]
+        },
+    ]
+};
